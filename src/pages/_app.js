e7e7ae745464b09d53a86b34fa2ce0e6e59f94cc
LucasMoya18 <lucasmoya.david@gmail.com>
2025-09-12 15:23:37 -0300

    tatodobug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,4 @@
 import '../styles/globals.css';
-import cors from "cors";
-
-app.use(cors({
-  origin: "https://gestioncitas.vercel.app", // frontend en Vercel
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
